f3b768057c4358f884a8a20799aad1bfdadfe4b0
Pasindu Perera <pasindup@leapset.com>
2015-01-22 02:43:05 +0530

    code cleanup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,14 +3,3 @@
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "scripts/contentscript.js"});
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-  console.log('\'Allo \'Allo! Event Page for Browser Action');
-});
